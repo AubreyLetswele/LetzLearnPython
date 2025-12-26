8d4aea6b12e291af4d14a1e4b11af97aeb279c63
Aubrey Letswele <aubreyletz8@gmail.com>
2025-12-26 13:51:16 +0200

    Add RSA ID decoder to calculate age and gender based on user input

--- FirstThingsFirst/intro.py
+++ FirstThingsFirst/intro.py
@@ -1,3 +1,28 @@
+#This piece of code will have all basics combined to get started with Python.
+
+# 1. Declaring variables and printing them together.
 firstline="Hello Python"
 secondline="Hello World"
-print(firstline +", " + secondline)
+year=2025
+print(firstline +", " + secondline + " " + str(year))
+
+
+# 2. getting datatypes and conditional booleans.
+name = "Aubrey"
+age = 21
+print(type(name))  
+print(isinstance(age, int))
+
+
+
+# 3. Simple math.
+x=6
+y=4
+
+z1=x+y
+z2=x*y
+z3=x/y
+z4=x-y
+z5=x**y
+z6=x%y
+z7=round(x//y,1) #round to 1 decimal place.
